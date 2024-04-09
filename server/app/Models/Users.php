0bbfea695fc22c20eef6178f45867148f4e59498
Anjeza Gashi <108656875+anjezagashi@users.noreply.github.com>
2024-04-10 00:53:13 +0200

    SG-5-Created users table migration and Users model

---
+++ server/app/Models/Users.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Users extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'username',
+        'email',
+        'salt',
+        'password',
+        'phone_number',
+        'type',
+    ];
+}
