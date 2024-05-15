711571ae30666e9bf9d701fc773fa9ebbc8cd3c6
Dafina Sadiku <dafinaasadiku@gmail.com>
2024-05-15 14:19:54 +0200

    SG-5: Created Careers, ContractTypes, PropertyTypes models and migration

--- server/app/Models/ContractTypes.php
+++ server/app/Models/ContractTypes.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Abilities extends Model
+class ContractTypes extends Model
 {
     use HasFactory;
 }
