6f994569bbab2db6d9c6032f56c22b4f5d2746bd
daf1na <dafinabalaj345@gmail.com>
2024-05-15 15:09:43 +0200

    SG-5: Created FAQs and Salaries models and migrations

---
+++ server/database/migrations/2024_05_15_130208_create_salaries_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('salaries', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id'); 
+            $table->string('position');
+            $table->string('month');
+            $table->decimal('wage_per_hour', 8, 2);
+            $table->integer('hours');
+            $table->integer('extra_hours');
+            $table->decimal('bonus', 8, 2);
+            $table->decimal('total', 10, 2);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('salaries');
+    }
+};
