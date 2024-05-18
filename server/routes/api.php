2d77d9bd9a436c565b22fff84af8bd7c895b3a87
daf1na <dafinabalaj345@gmail.com>
2024-05-19 00:34:58 +0200

    SG-11: Implemented form submission handling for Careers Page

--- server/routes/api.php
+++ server/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PropertyController;
+use App\Http\Controllers\CareersController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,3 +11,7 @@ Route::get('/user', function (Request $request) {
 
 
 Route::get('/properties', [PropertyController::class, 'getProperties']);
+Route::post('/careers', [CareersController::class, 'store']);
+
+
+
