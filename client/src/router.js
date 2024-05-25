3ce0de51043d4008d542686f28e7a6df0878daa2
bl3rt4 <blerta.aazemi@gmail.com>
2024-05-25 12:29:27 +0200

    SG-16: Displayed Property Details and added Review Section

--- client/src/router.js
+++ client/src/router.js
@@ -1,32 +1,33 @@
-
-
-import {createRouter, createWebHistory} from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import Register from './views/Register.vue';
 import Login from './views/Login.vue';
 import Careers from './views/Careers.vue';
 import Home from './views/Home.vue';
-import Agents from './views/Agents.vue'; 
+import Agents from './views/Agents.vue';
 import Properties from './views/Properties.vue';
 import Contact from './views/Contact.vue';
 import AboutUs from './views/AboutUs.vue';
-
+import PropertyDetails from './components/PropertyDetails.vue'; // Relative path
 
 const routes = [
-    { path: '/', component: Home },
-
-    { path: '/register',component:Register},
-    { path: '/login', component: Login },
-    { path: '/careers', component: Careers },
-    { path: '/agents', component: Agents },
-    { path: '/properties', component: Properties },
-    { path: '/contact', component: Contact },
-    { path: '/aboutus', component: AboutUs},
-
-];
+  { path: '/', component: Home },
+  { path: '/register', component: Register },
+  { path: '/login', component: Login },
+  { path: '/careers', component: Careers },
+  { path: '/agents', component: Agents },
+  { path: '/properties', component: Properties },
+  { path: '/contact', component: Contact },
+  { path: '/aboutus', component: AboutUs },
+  {
+    path: '/property/:id',
+    name: 'PropertyDetails',
+    component: PropertyDetails
+  }
+  ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
-  });
-  
-  export default router;
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
